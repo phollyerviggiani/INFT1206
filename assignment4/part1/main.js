14da7e810bf73ca4193c2b39f23cc5b4f4e14b20
phollyerviggiani <patrick.hollyerviggiani@dcmail.ca>
2024-03-21 16:37:54 -0400

    Assignment 4 Part 1 - 4th Commit
    added steps 3/4 for this

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -9,8 +9,37 @@ function randomValueFromArray(array) {
 }
 
 // Step 2
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ['Willy the Goblin' , 'Big Daddy' , 'Father Christmas'];
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
+// Step 3
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+    // Step 4
+    newStory = newStory
+        .replaceAll(':insertx:', xItem)
+        .replace(':inserty:', yItem)
+        .replace(':insertz:', zItem);
+
+    if (customName.value !== '') {
+        const name = customName.value;
+
+    }
+
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300);
+        const temperature = Math.round(94);
+
+    }
+
+    story.textContent = '';
+    story.style.visibility = 'visible';
+}
