99e0828bf4811ac37e19a87e7445a760e48501ad
phollyerviggiani <patrick.hollyerviggiani@dcmail.ca>
2024-03-22 10:16:16 -0400

    Assignment 4 Part 3 - Final Commit
    Just getting rid of some spaces and formatting

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -60,15 +60,13 @@ class Ball {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-  
+
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
         }
       }
     }
   }
-  
-  
 }
 
 const balls = [];
