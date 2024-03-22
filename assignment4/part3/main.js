ff2f84691262d256cf936fb4d4a2042276fd99f4
phollyerviggiani <patrick.hollyerviggiani@dcmail.ca>
2024-03-22 10:11:49 -0400

    Assignment 4 Part 3 - Commits
    Added collision detection to the balls

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -54,6 +54,20 @@ class Ball {
     ctx.fill();
   }
 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
   
 }
 
@@ -82,6 +96,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
