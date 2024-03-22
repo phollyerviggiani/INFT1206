2334eef316713fe56f4e73b04cd25039b51a2394
phollyerviggiani <patrick.hollyerviggiani@dcmail.ca>
2024-03-22 10:55:43 -0400

    Assignment 4 Part 2 - 3rd Commit
    Added the array imageArray and altText

--- assignment4/part2/main.js
+++ assignment4/part2/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = ['An eyeball', 'Beautiful landscape', 'Floral Artpiece', 'Egyptian Anubis', 'Butterfly'];
 
 /* Looping through images */
 
