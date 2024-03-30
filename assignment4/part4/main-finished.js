e94adb1d7ca6a47dba4e6bcb12c2350f4872cc48
Patrick Hollyer-Viggianigit config --global user.email patrick.hollyerviggiani@dcmail.ca <156444412+phollyerviggiani@users.noreply.github.com>
2024-03-30 16:52:31 -0400

    Assignemnt 4 Part 4 - 4th Commit
    Added the evil circle class, extending it from shape

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -65,7 +65,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -75,6 +75,34 @@ class Ball extends Shape {
         }
       }
     }
+  }
+}
+
+class EvilCircle extends Shape {
+
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener('keydown', (e) => {
+      switch (e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+          
+      }
+    });
+
   }
 }
 
